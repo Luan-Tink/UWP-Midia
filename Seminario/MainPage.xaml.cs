64160b19be05cfb1cf1db1b7dd6e8ebb17683f9a
Luan Barbosa <Luan Barbosa>
2017-02-15 20:34:53 -0300

    init

---
+++ Seminario/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using Windows.Media.Capture;
+using System.Threading.Tasks;
+using Windows.System.Display;
+using Windows.Graphics.Display;
+using Windows.Media.Playback;
+using Windows.Media.Core;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Seminario
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        MediaCapture _mediaCapture;
+        bool _isPreviewing;
+        DisplayRequest _displayRequest;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            StartPreviewAsync();
+            
+            //MediaPlayer _mediaPlayer = new MediaPlayer();
+            //_mediaPlayer.Source = MediaSource.CreateFromUri(new Uri("ms-appx:///Assets/video.wmv"));
+            //_mediaPlayerElement.SetMediaPlayer(_mediaPlayer);
+
+            //_mediaPlayerElement.Source = MediaSource.CreateFromUri(new Uri("ms-appx:///Assets/example_video.mkv"));
+            //_mediaPlayer = _mediaPlayerElement.MediaPlayer;
+            //_mediaPlayer.Play();
+        }
+
+        private async Task StartPreviewAsync()
+        {
+            try
+            {
+
+                _mediaCapture = new MediaCapture();
+                await _mediaCapture.InitializeAsync();
+
+                PreviewControl.Source = _mediaCapture;
+                await _mediaCapture.StartPreviewAsync();
+                _isPreviewing = true;
+
+                _displayRequest.RequestActive();
+                DisplayInformation.AutoRotationPreferences = DisplayOrientations.Landscape;
+
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // This will be thrown if the user denied access to the camera in privacy settings
+                System.Diagnostics.Debug.WriteLine("The app was denied access to the camera");
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("MediaCapture initialization failed. {0}", ex.Message);
+            }
+        }
+
+    }
+}
